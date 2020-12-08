3c0764002803b1458bb4ce13bf0c23acef371a8e
lwlean <lwlean@163.com>
2020-12-08 14:35:38 +0800

    first commit up things client

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	yunIp: '121.4.75.161',
+	localIp: '127.0.0.1',
+	port: '4000',
+	domain: 'upthings',
+}
---
+++ upthings.js
@@ -0,0 +1,64 @@
+#!node
+const fs = require('fs');
+const path = require('path');
+const config = require('./config');
+const http = require('http');
+
+// "use strict"
+/*
+ * author: llw
+ * change: 更改为可以提交文件，也可以提交一个字符串, 增加便利性
+ */
+(() =>{
+	const param = process.argv[2];	
+	let content = fs.existsSync(param) ? fs.readFileSync(path.resolve(param)) : param;
+	uploadThings(content);
+})();
+
+function uploadThings(content) {
+	try {
+		content += '\n';
+		const contentOfBase64 = Buffer.from(content.toString()).toString('base64');	
+		const url = getUrl() + 'filename=' + createFileName() + '&content=' + contentOfBase64;
+		console.log('final url:', url);
+		const req = http.request(url, (res) => {
+			res.on('data', (chunk) => {
+				console.log('chunk: ', chunk);
+			});
+			res.on('end', () => {
+				console.log('upthings over');
+			});
+		});
+
+		req.on('error', function(e) {
+			console.log('request cat error:', e);
+		});
+		
+		req.end();
+	} catch(e) {
+		console.error('upload things error:', e);
+	}
+}
+
+
+function createFileName() {
+	const date = new Date();
+	const year = date.getFullYear().toString();
+	const month = addZero(date.getMonth() + 1);
+	const day = addZero(date.getDate());
+	return year + month + day + '.md';	
+}
+
+function addZero(num) {
+	const len = num.toString().length;
+	if (len === 2) {
+		return num.toString();
+	}
+	return '0' + num.toString()
+}
+
+function getUrl() {
+	const ip = config.localIp;
+	//const ip = config.yunIp; 
+	return 'http://' + ip + ':' + config.port + '/' + config.domain + '/'; 
+}
